6081822f7b34db200ab4eab008608f569c8212af
Sandeep singh <sandy180388@gmail.com>
2022-09-19 16:43:07 +0530

    [sandeep] added addcontact in UC_1

---
+++ Address_Book_System/AddressBook.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_System
+{
+    class AddressBook
+    {
+        public static List<NewMember> contactList = new List<NewMember>();
+        public static void AddaPerson()
+        {
+            NewMember newMember = new NewMember();
+            Console.Write("Enter First Name: ");
+            newMember.firstname = Console.ReadLine();
+            Console.Write("Enter Last Name: ");
+            newMember.lastname = Console.ReadLine();
+            Console.Write("Enter Phone Number: ");
+            newMember.phonenumber = Console.ReadLine();
+            Console.Write("Enter Address: ");
+            newMember.Address = Console.ReadLine();
+            Console.Write("Enter City: ");
+            newMember.City = Console.ReadLine();
+            Console.Write("Enter State: ");
+            newMember.State = Console.ReadLine();
+            Console.Write("Enter Pincode: ");
+            newMember.pincode = Console.ReadLine();
+            contactList.Add(newMember);
+        }
+        public static void PrintPerson(NewMember member)
+        {
+            Console.WriteLine("First Name: " + member.firstname);
+            Console.WriteLine("Last Name: " + member.lastname);
+            Console.WriteLine("Phone Number: " + member.phonenumber);
+            Console.WriteLine("Address" + member.Address);
+            Console.WriteLine("City: " + member.City);
+            Console.WriteLine("State: " + member.State);
+            Console.WriteLine("Pincode: " + member.pincode);
+            Console.WriteLine("");
+
+        }
+        public static void ListContactPeople()
+        {
+            if (contactList.Count > 0)
+            {
+                Console.WriteLine("The Contact List : ");
+                foreach (var member in contactList)
+                {
+                    PrintPerson(member);
+                }
+
+            }
+            else
+            {
+
+                Console.WriteLine("Your address book is empty.");
+                return;
+            }
+        }
+
+    }
+}
---
+++ Address_Book_System/NewMember.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_System
+{
+    class NewMember
+    {
+        public string firstname
+        {
+            get;
+            set;
+        }
+        public string lastname
+        {
+            get;
+            set;
+        }
+        public string Address
+        {
+            get;
+            set;
+        }
+        public string City
+        {
+            get;
+            set;
+        }
+        public string State
+        {
+            get;
+            set;
+        }
+        public string pincode
+        {
+            get;
+            set;
+        }
+        public string phonenumber
+        {
+            get;
+            set;
+        }
+    }
+}
