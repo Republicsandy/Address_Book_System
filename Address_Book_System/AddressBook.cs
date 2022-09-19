4e8536a55608b13eb51d76877599c5c023286666
Sandeep singh <70006683+Republicsandy@users.noreply.github.com>
2022-09-19 17:19:08 +0530

    Update AddressBook.cs

--- Address_Book_System/AddressBook.cs
+++ Address_Book_System/AddressBook.cs
@@ -40,6 +40,83 @@ namespace Address_Book_System
             Console.WriteLine("");
 
         }
+        
+         public static void Modify()
+        {
+            if (contactList.Count > 0)
+            {
+                Console.WriteLine("Enter the First name of the contact to modify:");
+                string target = Console.ReadLine();
+                foreach (var member in contactList)
+                {
+                    if (member.firstname.ToLower() == target.ToLower())
+                    {
+                        while (true)
+                        {
+                            Console.WriteLine("Enter the option to modify the property: ");
+                            Console.WriteLine("Enter 1 to Change First name ");
+                            Console.WriteLine("Enter 2 to Change Last name ");
+                            Console.WriteLine("Enter 3 to Change Phone Number ");
+                            Console.WriteLine("Enter 4 to Change Address ");
+                            Console.WriteLine("Enter 5 to Change City ");
+                            Console.WriteLine("Enter 6 to Change State ");
+                            Console.WriteLine("Enter 7 to Change Pincode ");
+                            Console.WriteLine("Enter 8 to Exit ");
+                            int option = Convert.ToInt32(Console.ReadLine());
+                            switch (option)
+                            {
+                                case 1:
+                                    Console.WriteLine("Enter the New First Name: ");
+                                    member.firstname = Console.ReadLine();
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Enter the New Last Name: ");
+                                    member.lastname = Console.ReadLine();
+                                    break;
+                                case 3:
+                                    Console.WriteLine("Enter the New Phone Number: ");
+                                    member.phonenumber = Console.ReadLine();
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Enter the New Address: ");
+                                    member.Address = Console.ReadLine();
+                                    break;
+                                case 5:
+                                    Console.WriteLine("Enter the New City: ");
+                                    member.City = Console.ReadLine();
+                                    break;
+                                case 6:
+                                    Console.WriteLine("Enter the New State: ");
+                                    member.State = Console.ReadLine();
+                                    break;
+                                case 7:
+                                    Console.WriteLine("Enter the New Pin Code: ");
+                                    member.pincode = Console.ReadLine();
+                                    break;
+                                case 8:
+                                    return;
+
+                            }
+
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter the valid name!");
+                    }
+
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("Your Address Book is empty!");
+            }
+
+        }
+        
         public static void ListContactPeople()
         {
             if (contactList.Count > 0)
